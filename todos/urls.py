7c60f54588b2342a4a8a7fbeb4253906f9fc5553
WesleyBule <bulewesley7@gmail.com>
2025-11-01 23:00:03 +0200

    feat: todo_create view

--- todos/urls.py
+++ todos/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.todo_list , name='todoList')
+    path('',views.todo_list , name='todoList'),
+    path('add/', views.todo_create , name='todoCreate'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Todo
 
 
@@ -6,3 +6,20 @@ def todo_list(request):
     todos = Todo.objects.all()
     template_name = 'todos/home.html'
     return render(request,template_name ,{'todos':todos})
+
+def todo_create(request):
+    template_name = 'todos/create.html'
+    if request.method == "POST":
+        title = request.POST.get('title')
+        created = request.POST.get('created')
+        deadline = request.POST.get('deadline')
+        completed = request.POST.get('completed')
+
+        Todo.objects.create(
+            title=title,
+            created = created,
+            deadline = deadline,
+            completed = completed
+        )
+        return redirect('todoList')
+    return render(request,template_name)
